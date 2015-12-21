b52b6904daa05023f46cd576ba992c99400640a6
upunik <alienkd@outlook.com>
2015-12-20 19:53:09 -0600

    Change with front-end

--- myenv/bin/django-admin.py
+++
@@ -1,4 +0,0 @@
-#!/Users/qzhang72/APPCS411/myenv/bin/python
-# EASY-INSTALL-SCRIPT: 'Django==1.8.6','django-admin.py'
-__requires__ = 'Django==1.8.6'
-__import__('pkg_resources').run_script('Django==1.8.6', 'django-admin.py')
